363c5a0e605dbd277ac530b0d589fd419f9a7788
Ing-ed <emidovichi@gmail.com>
2023-03-08 17:55:31 -0300

    PreEntrega2Dovichi

--- src/Components/ItemDetail/index.jsx
+++ src/Components/ItemDetail/index.jsx
@@ -3,15 +3,17 @@ import "./estilo.css"
 
 
 export function ItemDetail({producto}){
-    console.log(producto)
+    console.log(typeof producto, "aca")
     return(
         <div className="itemDetail">
             <ul>
                 <li><p>{producto.title}</p></li>
                 <li><img src={producto.picture}/></li>
-                <li><p>{producto.price}</p></li>
+                <li><p>${producto.price}</p></li>
             </ul>
+            {/* <hr/> */}
             <p>{producto.description}</p>
+            {/* </ul> */}
         </div>
     )
 }
--- src/Components/Mocks/productos.js
+++ src/Components/Mocks/productos.js
@@ -2,7 +2,8 @@ import Pic from '../../assets/18f2550.png'
 import Lpc from '../../assets/1769.png'
 import Esp from '../../assets/esp32.png'
 export let productos = [
-    {id:"Pic",title:"18F2550",description:"Pic micro description",picture: Pic, price:"price"},
+    {id:"Pic",title:"18F2550",description:"Descripcion:\n"+
+    `Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quisquam similique veritatis eius porro. Numquam, facilis! Distinctio quia vero quam deserunt ipsum quo consequatur, minus hic pariatur rem, repellendus ea.`,picture: Pic, price:24.75},
     {id:"Esp",title:"Esp32",description:"Esp32 description",picture:Esp, price:"price"},
     {id:"Lpc",title:"Lpc1769",description:"Lpc1769 description",picture:Lpc, price:"price"},
 ]
